64b7ba0f7fe22a2ef8c773982d6ac2e8847d6854
Johnny Mast <mastjohnny@gmail.com>
2025-10-27 18:28:18 +0100

    Update build scripts to use FetchContent for LibGraphics
    Updated CMake to fetch LibGraphics directly from its GitHub repository instead of using a local subdirectory. Also adjusted minimum required CMake version to 3.5 to support FetchContent functionality.

--- cmake/Graphics.cmake
+++ cmake/Graphics.cmake
@@ -1,11 +1,30 @@
+#set(LIBGRAPHICS_ENABLE_TESTS OFF CACHE BOOL "Disable LibGraphics tests")
+#
+## Add LibGraphics once
+#add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/../Graphics" Graphics_screenshots_build)
+#
+## Optionally expose headers (if not already PUBLIC in LibGraphics)
+#message(STATUS "Including LibGraphics headers at ${CMAKE_CURRENT_SOURCE_DIR}/../Graphics/include")
+#include_directories("${CMAKE_CURRENT_SOURCE_DIR}/../Graphics/include")
+#
+#
+#target_link_libraries(Screenshots PUBLIC Graphics)
+
 set(LIBGRAPHICS_ENABLE_TESTS OFF CACHE BOOL "Disable LibGraphics tests")
 
-# Add LibGraphics once
-add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/../Graphics" Graphics_screenshots_build)
+# Use LibGraphics directly from GitHub if not installed locally
+include(FetchContent)
 
-# Optionally expose headers (if not already PUBLIC in LibGraphics)
-message(STATUS "Including LibGraphics headers at ${CMAKE_CURRENT_SOURCE_DIR}/../Graphics/include")
-include_directories("${CMAKE_CURRENT_SOURCE_DIR}/../Graphics/include")
+FetchContent_Declare(LibGraphics
+        GIT_REPOSITORY https://github.com/johnnymast/LibGraphics.git
+        GIT_TAG master
+)
 
+# Downloaded content will be available to the current CMake project
+FetchContent_MakeAvailable(LibGraphics)
 
-target_link_libraries(Screenshots PUBLIC Graphics)
+# LibGraphics provides a target named LibGraphics
+# Link Screenshots against LibGraphics instead of a local subdirectory
+# (The previous add_subdirectory approach is replaced with FetchContent)
+# Optionally, LibGraphics headers are automatically included when the target is linked.
+target_link_libraries(Screenshots PUBLIC LibGraphics)
