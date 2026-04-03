01b76d364ede7ab1e6fe67da3ba018b0147fd6b0
Johnny Mast <mastjohnny@gmail.com>
2026-04-03 18:55:58 +0200

    Replaced FetchContent with find_package for LibGraphics in Graphics.cmake

--- cmake/Graphics.cmake
+++ cmake/Graphics.cmake
@@ -1,12 +1,13 @@
-set(LIBGRAPHICS_ENABLE_TESTS OFF CACHE BOOL "Disable LibGraphics tests")
-
-include(FetchContent)
-
-FetchContent_Declare(LibGraphics
-        GIT_REPOSITORY https://github.com/johnnymast/LibGraphics.git
-        GIT_TAG master
-)
-
-FetchContent_MakeAvailable(LibGraphics)
-
-target_link_libraries(LibScreenshots PUBLIC LibGraphics)
+#set(LIBGRAPHICS_ENABLE_TESTS OFF CACHE BOOL "Disable LibGraphics tests")
+#
+#include(FetchContent)
+#
+#FetchContent_Declare(LibGraphics
+#        GIT_REPOSITORY https://github.com/johnnymast/LibGraphics.git
+#        GIT_TAG master
+#)
+#
+#FetchContent_MakeAvailable(LibGraphics)
+#
+#target_link_libraries(LibScreenshots PUBLIC LibGraphics)
+find_package(LibGraphics REQUIRED)
